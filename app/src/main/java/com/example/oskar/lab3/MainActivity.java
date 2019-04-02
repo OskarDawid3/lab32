b737c612b3f33759b750a37ec9b0611f4386dfcb
Oskar Dawid <o.dawid3@gmail.com>
2019-04-02 23:23:55 +0200

    finish lab6

--- app/src/main/java/com/example/oskar/lab3/MainActivity.java
+++ app/src/main/java/com/example/oskar/lab3/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         this.db = new MySQLite(this);
 
-        String[] values = new String[] { "Pies",
+        String[] values = new String[]{"Pies",
                 "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
-                "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+                "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
 
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
@@ -37,16 +40,37 @@ public class MainActivity extends AppCompatActivity {
                 this,
                 android.R.layout.simple_list_item_2,
                 db.lista(),
-                new String[] {"_id", "gatunek"},
-                new int[] {android.R.id.text1,
+                new String[]{"_id", "gatunek"},
+                new int[]{android.R.id.text1,
                         android.R.id.text2},
 
                 SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
         );
 
-        ListView listview = (ListView) findViewById(
-                R.id.listView );
+        ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 2);
+            }
+        });
+
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                db.usun(name.getText().toString());
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     public void nowyWpis(MenuItem mi)
@@ -71,9 +95,16 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==1 && resultCode==RESULT_OK)
         {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
+
+            Animal nowy = (Animal)
+                    extras.getSerializable("nowy");
+
+            this.db.dodaj(nowy);
+
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
+
+
         }
     }
 
